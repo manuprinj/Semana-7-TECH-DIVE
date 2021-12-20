07b2cbe58ed2a995df7caf1553735a7efc3719d4
manuprinj <manuelaprinjacinto@gmail.com>
2021-12-19 23:40:01 -0300

    Ex 5
    Change-Id: I5c4dde62f35d119af852b7a418c5709308764510

---
+++ src/model/Operario.java
@@ -0,0 +1,33 @@
+package model;
+
+
+public class Operario extends Empregado {
+
+    private double valorProducao;
+    private double porcentagemComissao;
+
+    public double calcularComissao() {
+        return valorProducao * porcentagemComissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + calcularComissao();
+    }
+
+    public double getValorProducao() {
+        return valorProducao;
+    }
+
+    public void setValorProducao(double valorProducao) {
+        this.valorProducao = valorProducao;
+    }
+
+    public double getPorcentagemComissao() {
+        return porcentagemComissao;
+    }
+
+    public void setPorcentagemComissao(double porcentagemComissao) {
+        this.porcentagemComissao = porcentagemComissao;
+    }
+}
---
+++ src/test/OperarioTest.java
@@ -0,0 +1,19 @@
+package test;
+
+
+import model.Operario;
+
+
+public class OperarioTest {
+
+    public static void main(String[] args) {
+        Operario operario = new Operario();
+
+        operario.setSalarioBase(500);
+        operario.setImposto(100);
+        operario.setValorProducao(100);
+        operario.setPorcentagemComissao(0.1);
+
+        System.out.println("O valor do salário é: R$ " + operario.calcularSalario());
+    }
+}
